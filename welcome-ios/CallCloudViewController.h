acbcc11a1918997d66782f1b8d4b365e50ee957c
Daniel Passos <daniel@passos.me>
2016-03-17 12:28:54 -0300

    Add call cloud feature

--- welcome-ios/CallCloudViewController.h
+++ welcome-ios/CallCloudViewController.h
@@ -5,5 +5,8 @@
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
 
 @property (weak, nonatomic) IBOutlet UIButton *callButton;
+@property (weak, nonatomic) IBOutlet UITextView *result;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loading;
+
 
 @end
--- welcome-ios/CallCloudViewController.m
+++ welcome-ios/CallCloudViewController.m
@@ -1,5 +1,6 @@
 #import "CallCloudViewController.h"
 #import "SWRevealViewController.h"
+#import "FH.h"
 
 @interface CallCloudViewController ()
 
@@ -14,10 +15,67 @@
     
     self.automaticallyAdjustsScrollViewInsets = NO;
     
+    _loading.hidden = YES;
+    
 }
 
 - (IBAction)callCloud:(id)sender {
+    
     NSLog(@"Call cloud was clicked");
+    
+    // Block user interaction
+    UIView *overlayView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    overlayView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+    [[UIApplication sharedApplication].keyWindow addSubview:overlayView];
+    
+    // Start UIActivityIndicatorView
+    _loading.hidden = NO;
+    [_loading startAnimating];
+    
+    // Fire cloud request
+    FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:nil];
+    
+    [req execAsyncWithSuccess:^(FHResponse * res) {
+        // Response
+        NSLog(@"Response: %@", res.rawResponseAsString);
+        NSLog(@"Text: %@", [res.parsedResponse objectForKey:@"text"]);
+        
+        // Show result
+        _result.text = [res.parsedResponse objectForKey:@"text"];
+        
+        // Remove block interaction layer
+        [overlayView removeFromSuperview];
+        
+        // Stop UIActivityIndicatorView
+        [_loading stopAnimating];
+        _loading.hidden = YES;
+        
+    } AndFailure:^(FHResponse * res){
+
+        NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+        
+        // Remove block interaction layer
+        [overlayView removeFromSuperview];
+        
+        // Stop UIActivityIndicatorView
+        [_loading stopAnimating];
+        _loading.hidden = YES;
+        
+        // Show user alert
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Failure"
+                                     message:@"Oops, something went wrong"
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* okAction = [UIAlertAction
+                                   actionWithTitle:@"Ok, roger that!"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:nil];
+    
+    }];
 }
 
 @end
