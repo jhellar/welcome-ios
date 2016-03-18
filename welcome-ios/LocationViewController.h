170187840e659422c587cb0acbd5b7beafbe7686
Daniel Passos <daniel@passos.me>
2016-03-18 07:55:33 -0300

    Add location/weather feature

--- welcome-ios/LocationViewController.h
+++ welcome-ios/LocationViewController.h
@@ -4,6 +4,15 @@
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
 
-@property (weak, nonatomic) IBOutlet UIButton *locationButton;
+@property (weak, nonatomic) IBOutlet UITextField *latitude;
+@property (weak, nonatomic) IBOutlet UITextField *longitude;
+@property (weak, nonatomic) IBOutlet UIButton *weatherButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *weatherDescription;
+@property (weak, nonatomic) IBOutlet UILabel *weatherDate;
+@property (weak, nonatomic) IBOutlet UILabel *weatherLow;
+@property (weak, nonatomic) IBOutlet UILabel *weatherHigh;
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loading;
 
 @end
--- welcome-ios/LocationViewController.m
+++ welcome-ios/LocationViewController.m
@@ -1,5 +1,6 @@
 #import "LocationViewController.h"
 #import "SWRevealViewController.h"
+#import "FH.h"
 
 @interface LocationViewController ()
 
@@ -14,10 +15,81 @@
     
     self.automaticallyAdjustsScrollViewInsets = NO;
     
+    _loading.hidden = YES;
+    
 }
 
-- (IBAction)geMytLocation:(id)sender {
+- (IBAction)retrieveWeatherInfo:(id)sender {
+
     NSLog(@"Get My Location was clicked");
+    
+    // Block user interaction
+    UIView *overlayView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    overlayView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+    [[UIApplication sharedApplication].keyWindow addSubview:overlayView];
+    
+    // Start UIActivityIndicatorView
+    _loading.hidden = NO;
+    [_loading startAnimating];
+    
+    // Fire cloud request
+    NSDictionary *args = [NSDictionary dictionaryWithObjectsAndKeys:_latitude.text, @"lat", _longitude.text, @"lon", nil];
+    
+    FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/getWeather" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
+    
+    [req execAsyncWithSuccess:^(FHResponse * res) {
+
+        NSLog(@"Response: %@", res.rawResponseAsString);
+        
+        NSDictionary *responseDictionary = res.parsedResponse;
+        NSArray *responseData = [responseDictionary objectForKey:@ "data"];
+        NSDictionary *data = responseData[0];
+
+        NSLog(@"Data: %@", data);
+        
+        // Show result
+        _weatherDescription.text = [data objectForKey:@"desc"];
+        _weatherDate.text = [data objectForKey:@"date"];
+        _weatherLow.text = [data objectForKey:@"low"];
+        _weatherHigh.text = [data objectForKey:@"high"];
+        
+        NSLog(@"Description: %@", [data objectForKey:@"desc"]);
+        
+        // Remove block interaction layer
+        [overlayView removeFromSuperview];
+        
+        // Stop UIActivityIndicatorView
+        [_loading stopAnimating];
+        _loading.hidden = YES;
+        
+    } AndFailure:^(FHResponse * res){
+        
+        NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+        
+        // Remove block interaction layer
+        [overlayView removeFromSuperview];
+        
+        // Stop UIActivityIndicatorView
+        [_loading stopAnimating];
+        _loading.hidden = YES;
+        
+        // Show user alert
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Failure"
+                                     message:@"Oops, something went wrong"
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* okAction = [UIAlertAction
+                                   actionWithTitle:@"Ok, roger that!"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+    }];
+
 }
 
 @end
+
