64a4ae541649d93361847faf59f86888852596e3
Daniel Passos <daniel@passos.me>
2016-03-18 11:57:53 -0300

    Add native app info feature

--- welcome-ios/NativeAppInfoViewController.h
+++ welcome-ios/NativeAppInfoViewController.h
@@ -4,4 +4,11 @@
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
 
+@property (weak, nonatomic) IBOutlet UILabel *couldAppName;
+@property (weak, nonatomic) IBOutlet UILabel *couldDomain;
+@property (weak, nonatomic) IBOutlet UILabel *couldEnv;
+@property (weak, nonatomic) IBOutlet UILabel *couldPort;
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loading;
+
 @end
--- welcome-ios/NativeAppInfoViewController.m
+++ welcome-ios/NativeAppInfoViewController.m
@@ -1,5 +1,6 @@
 #import "NativeAppInfoViewController.h"
 #import "SWRevealViewController.h"
+#import "FH.h"
 
 @interface NativeAppInfoViewController ()
 
@@ -11,6 +12,68 @@
     
     _sidebarButton.target = self.revealViewController;
     _sidebarButton.action = @selector(revealToggle:);
+    
+    _loading.hidden = YES;
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    
+    // Block user interaction
+    UIView *overlayView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    overlayView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+    [[UIApplication sharedApplication].keyWindow addSubview:overlayView];
+    
+    // Start UIActivityIndicatorView
+    _loading.hidden = NO;
+    [_loading startAnimating];
+    
+    // Fire cloud request
+    FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/getFhVars" WithMethod:@"POST" AndHeaders:nil AndArgs:nil];
+    
+    [req execAsyncWithSuccess:^(FHResponse * res) {
+        // Response
+        NSLog(@"Response: %@", res.rawResponseAsString);
+        
+        // Show result
+        _couldAppName.text = [res.parsedResponse objectForKey:@"appName"];
+        _couldDomain.text = [res.parsedResponse objectForKey:@"domain"];
+        _couldEnv.text = [res.parsedResponse objectForKey:@"env"];
+        _couldPort.text = [res.parsedResponse objectForKey:@"port"];
+        
+        // Remove block interaction layer
+        [overlayView removeFromSuperview];
+        
+        // Stop UIActivityIndicatorView
+        [_loading stopAnimating];
+        _loading.hidden = YES;
+        
+    } AndFailure:^(FHResponse * res){
+        
+        NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+        
+        // Remove block interaction layer
+        [overlayView removeFromSuperview];
+        
+        // Stop UIActivityIndicatorView
+        [_loading stopAnimating];
+        _loading.hidden = YES;
+        
+        // Show user alert
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Failure"
+                                     message:@"Oops, something went wrong"
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* okAction = [UIAlertAction
+                                   actionWithTitle:@"Ok, roger that!"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+    }];
     
 }
 
